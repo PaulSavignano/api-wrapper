a0f4e97b9ffbe1a8eb8d189ade7b81e0a83afb44
Paul Savignano <Paul.Savignano@gmail.com>
2016-10-15 11:49:14 -0700

    Add methods to MailChimpAPI class.

--- imports/modules/mailchimp.js
+++ imports/modules/mailchimp.js
@@ -7,11 +7,35 @@ class MailChimpAPI {
     this.apiKey = apiKey
     this.methods = {
       lists: {
-        subscribe() {
-
+        subscribe({ list_id, subscriber_hash }) {
+          return { type: 'PUT', endpoint: `/lists/${list_id}/members/${subscriber_hash}` }
         },
       },
     }
+  }
+  buildRequestArguments(type, options) {
+    const payload = { auth: `mailchimp:${this.apiKey}` }
+    if (type === 'GET') {
+      payload.params = options || {}
+    } else { 
+      payload.data = options || {}
+    }
+    return payload
+  }
+  request(action, options) {
+    const type = action.type
+    const url = `https://us14.api.mailchimp.com/3.0${action.endpoint}`
+    const args = this.buildRequestArguments(type, options)
+    const request = HTTP.call(type, url, args)
+    if (request.error) return request.error
+    return request
+  }
+  action(action, method, params) {
+    const methodToCall = this.methods[action][method](params)
+    return this.request(methodToCall, params)
+  }
+  lists(method, params) {
+    return this.action('lists', method, params)
   }
 }
 
